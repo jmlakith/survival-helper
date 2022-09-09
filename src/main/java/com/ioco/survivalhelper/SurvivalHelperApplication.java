8a7e1a0b134600f189237c845382baf3fab00b09
lakithd <lakith.dharmarathna@bukuwarung.com>
2022-09-09 11:26:25 +0530

    first commit to survive

---
+++ src/main/java/com/ioco/survivalhelper/SurvivalHelperApplication.java
@@ -0,0 +1,13 @@
+package com.ioco.survivalhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivalHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ioco/survivalhelper/SurvivalHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioco.survivalhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivalHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
